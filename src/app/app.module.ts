3e4732b2c804c6d3c0385f2d0989cd0fc5428c07
Ian Goldfarb <ian@supplyhub.com>
2017-04-02 13:28:18 -0700

    tested a few variations on how to set up the rooms, and I'm happy with the current model, now to consider the types of functionality to include

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AdventureComponent } from './adventure/adventure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdventureComponent
   ],
   imports: [
     BrowserModule,
